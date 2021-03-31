51fbe9397e738e4fd5016fa32bf43d5d66bbe7cf
Zhenni Qian <zhenni.qian913@gmail.com>
2021-03-31 23:46:52 +0200

    refactor: rewrite Home component in function with useState and useEffect

--- src/components/farmer/quote/Quotes.js
+++ src/components/farmer/quote/Quotes.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import AddQuote from './AddQuote';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import AddQuote from "./AddQuote";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import { Button, Card, CardDeck } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { Button, Card, CardDeck } from "react-bootstrap";
 
 export default class Quotes extends Component {
   state = {
@@ -13,66 +13,86 @@ export default class Quotes extends Component {
     responseMessage: null,
     quotes: [],
     isLoaded: false,
-  }
+  };
 
   componentDidMount() {
-    axios.get(`${process.env.REACT_APP_API_URL}/quotes?farmer=${this.props.user._id}`, {withCredentials: true})
-      .then(allQuotes => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/quotes?farmer=${this.props.user._id}`,
+        { withCredentials: true }
+      )
+      .then((allQuotes) => {
         this.setState({
           quotes: allQuotes.data,
           isLoaded: true,
-        })
+        });
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   }
 
   handleAddQuote = (data) => {
     this.setState({
       showAddForm: false,
-      responseMessage: data.message
+      responseMessage: data.message,
     });
 
     setTimeout(() => {
       this.setState({
-        responseMessage: null
-      })
+        responseMessage: null,
+      });
     }, 2000);
 
     this.props.updateList(data.newQuote, "quotes", true);
-  }
+  };
 
   render() {
     return (
       <Container>
-        <Row>{this.state.responseMessage && <p>{this.state.responseMessage}</p>}</Row>
-        {this.state.isLoaded ?
         <Row>
-          <CardDeck className="m-5">
-            {this.state.quotes.map(quote => {
-              return (
-                <Card key={quote._id} border="primary" style={{ width: '18rem', borderRadius: '4px' }}>
-                  <Card.Body>
-                    <Card.Title>{quote.field.fieldName}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Number of offers: {quote.offers.length}</Card.Subtitle>
-                    <Card.Text>
-                      {quote.service?.icon} {quote.service?.service}
-                      <br></br>
-                      {quote.date.slice(0, 10)}
-                    </Card.Text>
-                    <Link to={`/dashboard/quotes/${quote._id}`}>More details</Link>
-                  </Card.Body>
-                </Card>
-              )
-            })}
-          </CardDeck>
+          {this.state.responseMessage && <p>{this.state.responseMessage}</p>}
         </Row>
-        :
-        <Row>Loading...</Row>}
+        {this.state.isLoaded ? (
+          <Row>
+            <CardDeck className="m-5">
+              {this.state.quotes.map((quote) => {
+                return (
+                  <Card
+                    key={quote._id}
+                    border="primary"
+                    style={{ width: "18rem", borderRadius: "4px" }}
+                  >
+                    <Card.Body>
+                      <Card.Title>{quote.field.fieldName}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        Number of offers: {quote.offers.length}
+                      </Card.Subtitle>
+                      <Card.Text>
+                        {quote.service?.icon} {quote.service?.service}
+                        <br></br>
+                        {quote.date.slice(0, 10)}
+                      </Card.Text>
+                      <Link to={`/dashboard/quotes/${quote._id}`}>
+                        More details
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </CardDeck>
+          </Row>
+        ) : (
+          <Row>Loading...</Row>
+        )}
         <Row>
-          {this.state.showAddForm ?
-            <AddQuote onAddQuote={this.handleAddQuote} /> : <Button onClick={() => this.setState({showAddForm: true})}>Add a new quote</Button>}
+          {this.state.showAddForm ? (
+            <AddQuote onAddQuote={this.handleAddQuote} />
+          ) : (
+            <Button onClick={() => this.setState({ showAddForm: true })}>
+              Add a new quote
+            </Button>
+          )}
         </Row>
       </Container>
-    )
+    );
   }
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { authService } from "../auth/auth-service";
 import axios from "axios";
 
@@ -14,198 +14,198 @@ import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import CardColumns from "react-bootstrap/CardColumns";
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listContractors: [],
-    };
-  }
+export default function Home(props) {
+  const [listContractors, setListContractors] = useState([]);
 
-  fetchcontractorList = () => {
+  const fetchcontractorList = () => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/contractorlist`, {
         withCredentials: true,
       })
       .then((responseFromApi) => {
-        this.setState({
-          listContractors: [...responseFromApi.data],
-        });
+        setListContractors(responseFromApi.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  logoutUser = () => {
+  const logoutUser = () => {
     authService.logout().then(() => {
-      this.props.onLogout();
+      props.onLogout();
     });
   };
 
-  componentDidMount() {
-    this.fetchcontractorList();
-  }
+  useEffect(() => {
+    fetchcontractorList();
+  }, []);
 
-  render() {
-    return (
-      <>
-        <Navbar bg="dark">
-          <Navbar.Brand href="/" className="navbar-brand">
-            <div
-              className="logoAgroform d-inline-block align-top"
-              alt="agroform"
-            ></div>
-          </Navbar.Brand>
-          {this.props.userInSession ? (
-            <Nav.Link href="/dashboard">My dashboard</Nav.Link>
-          ) : null}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" />
-          <Nav className="mr-auto">
-            {this.props.userInSession ? (
-              <Button
-                className="m-1"
-                onClick={this.logoutUser}
-                variant="outline-warning"
-              >
-                Logout
+  return (
+    <>
+      <Navbar bg="dark">
+        <Navbar.Brand href="/" className="navbar-brand">
+          <div
+            className="logoAgroform d-inline-block align-top"
+            alt="agroform"
+          ></div>
+        </Navbar.Brand>
+        {props.userInSession ? (
+          <Nav.Link href="/dashboard">My dashboard</Nav.Link>
+        ) : null}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" />
+        <Nav className="mr-auto">
+          {props.userInSession ? (
+            <Button
+              className="m-1"
+              onClick={logoutUser}
+              variant="outline-warning"
+            >
+              Logout
+            </Button>
+          ) : (
+            <>
+              <Button className="m-1" variant="outline-primary" href="/login">
+                Login
               </Button>
-            ) : (
-              <>
-                <Button className="m-1" variant="outline-primary" href="/login">
-                  Login
-                </Button>
-                <Button className="m-1" href="/register">
-                  Register
-                </Button>
-              </>
-            )}
-          </Nav>
-        </Navbar>
+              <Button className="m-1" href="/register">
+                Register
+              </Button>
+            </>
+          )}
+        </Nav>
+      </Navbar>
 
-        <Container fluid className="pictureMain">
-          <Carousel>
-
-            <Carousel.Item interval={10000} className="containerCarousel">
-              <Carousel.Caption>
-                <Row>
+      <Container fluid className="pictureMain">
+        <Carousel>
+          <Carousel.Item interval={10000} className="containerCarousel">
+            <Carousel.Caption>
+              <Row>
                 <Col md={{ span: 8, offset: 2 }}>
-                    <div className="firstItemImg" alt="compareContractor"></div>
-                    <br />
-                  </Col>
-                </Row>
-                <Row className="overlay">
-                  <div className="borderFill"></div>
+                  <div className="firstItemImg" alt="compareContractor"></div>
+                  <br />
+                </Col>
+              </Row>
+              <Row className="overlay">
+                <div className="borderFill"></div>
 
-                  <Col className="text" md={{ span: 6, offset: 3 }}>
-                    <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
-                      WELCOME TO AGROFORM
-                    </h2>
-                    <p style={{ color: "#0C0704" }}>
-                      The innovative platform for Farmers and contractors
-                    </p>
-                  </Col>
-                </Row>
-              </Carousel.Caption>
-            </Carousel.Item>
+                <Col className="text" md={{ span: 6, offset: 3 }}>
+                  <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
+                    WELCOME TO AGROFORM
+                  </h2>
+                  <p style={{ color: "#0C0704" }}>
+                    The innovative platform for Farmers and contractors
+                  </p>
+                </Col>
+              </Row>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-            <Carousel.Item interval={10000} className="containerCarousel">
-              <Carousel.Caption>
-                <Row>
-                  <Col md={{ span: 8, offset: 2 }}>
-                    <div className="threeItemImg" alt="fields"></div>
-                  </Col>
-                </Row>
-                <Row className="overlay">
-                  <div className="borderFill"></div>
-                    <Col className="text" md={{ span: 6, offset: 3 }}>
-                      <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
-                        You want to have an overview of all your fields?
-                      </h2>
-                      <p style={{ color: "#0C0704" }}>
-                        Create an overview of all your fields and always see the
-                        complete working history.
-                      </p>
-                    </Col>
-                </Row>
-              </Carousel.Caption>
-            </Carousel.Item>
+          <Carousel.Item interval={10000} className="containerCarousel">
+            <Carousel.Caption>
+              <Row>
+                <Col md={{ span: 8, offset: 2 }}>
+                  <div className="threeItemImg" alt="fields"></div>
+                </Col>
+              </Row>
+              <Row className="overlay">
+                <div className="borderFill"></div>
+                <Col className="text" md={{ span: 6, offset: 3 }}>
+                  <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
+                    You want to have an overview of all your fields?
+                  </h2>
+                  <p style={{ color: "#0C0704" }}>
+                    Create an overview of all your fields and always see the
+                    complete working history.
+                  </p>
+                </Col>
+              </Row>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-            <Carousel.Item interval={10000} className="containerCarousel">
-              <Carousel.Caption>
-                <Row>
-                  <Col md={{ span: 3, offset: 4 }}>
-                    <div
-                      className="secondItemImg"
-                      alt="compareContractor"
-                    ></div>
-                    <br />
-                  </Col>
-                </Row>
-                <Row className="overlay">
-                  <div className="borderFill"></div>
-                    <Col className="text" md={{ span: 6, offset: 3 }}>
-                      <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
-                        Are you looking for the right contracting company?
-                      </h2>
-                      <p style={{ color: "#0C0704" }}>
-                        Find the right partner for your agricultural service
-                        quickly and easily with AGROFORM.
-                      </p>
-                    </Col>
-                </Row>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </Container>
-            
-        <Container>
-          <h2 className="m-5">Contractors nearby:</h2>
-          <CardColumns>
-            {this.state.listContractors.map((contractors) => {
-              return (
-                <Card style={{ width: "18rem" }}>
-                 
-                  <React.Fragment>
-                     <img alt="contractorImg" src={contractors.userImg} />
-                 </React.Fragment>
-                  <Card.Body style={{backgroundColor: "#663925", color: "#fff"}}>
-                    <Card.Title style={{textAlign:"center" }}><h3>{contractors.username}</h3></Card.Title>
-                  </Card.Body>
+          <Carousel.Item interval={10000} className="containerCarousel">
+            <Carousel.Caption>
+              <Row>
+                <Col md={{ span: 3, offset: 4 }}>
+                  <div className="secondItemImg" alt="compareContractor"></div>
+                  <br />
+                </Col>
+              </Row>
+              <Row className="overlay">
+                <div className="borderFill"></div>
+                <Col className="text" md={{ span: 6, offset: 3 }}>
+                  <h2 style={{ color: "#0C0704", paddingTop: "15px" }}>
+                    Are you looking for the right contracting company?
+                  </h2>
+                  <p style={{ color: "#0C0704" }}>
+                    Find the right partner for your agricultural service quickly
+                    and easily with AGROFORM.
+                  </p>
+                </Col>
+              </Row>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+
+      <Container>
+        <h2 className="m-5">Contractors nearby:</h2>
+        <CardColumns>
+          {listContractors.map((contractors) => {
+            return (
+              <Card style={{ width: "18rem" }}>
+                <React.Fragment>
+                  <img alt="contractorImg" src={contractors.userImg} />
+                </React.Fragment>
+                <Card.Body
+                  style={{ backgroundColor: "#663925", color: "#fff" }}
+                >
+                  <Card.Title style={{ textAlign: "center" }}>
+                    <h3>{contractors.username}</h3>
+                  </Card.Title>
+                </Card.Body>
+                <ListGroupItem>
+                  <p>Location:</p>
+                  <ul>
+                    <li>{contractors.street}</li>
+                    <li>{contractors.city}</li>
+                    <li>{contractors.country}</li>
+                  </ul>
+                </ListGroupItem>
+                <ListGroup className="list-group-flush">
                   <ListGroupItem>
-                    <p>Location:</p>
-                    <ul>
-                      <li>{contractors.street}</li>
-                      <li>{contractors.city}</li>
-                      <li>{contractors.country}</li>
-                    </ul>
+                    <p>Services:</p>
+                    {contractors.services.map((conServices) => {
+                      return (
+                        <p>
+                          {conServices.icon} {conServices.service}
+                        </p>
+                      );
+                    })}
                   </ListGroupItem>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <p>Services:</p>
-                      {contractors.services.map((conServices) => {
-                        return (
-                          <p>
-                            {conServices.icon} {conServices.service}
-                          </p>
-                        );
-                      })}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <p>Vehicles:</p>
-                      {contractors.vehicules.map((conVehicules) => {
-                        return <p>{conVehicules.vehicule}</p>;
-                      })}
-                    </ListGroupItem>
-                  </ListGroup>
-                </Card>
-              );
-            })}
-          </CardColumns>
-        </Container>
-        <footer style={{paddingBottom: "0px", backgroundColor: "black", textAlign: "center", margin: "auto", color: '#489B34'}}>agroform &#169; 2021</footer>
-      </>
-    );
-  }
+                  <ListGroupItem>
+                    <p>Vehicles:</p>
+                    {contractors.vehicules.map((conVehicules) => {
+                      return <p>{conVehicules.vehicule}</p>;
+                    })}
+                  </ListGroupItem>
+                </ListGroup>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+      <footer
+        style={{
+          paddingBottom: "0px",
+          backgroundColor: "black",
+          textAlign: "center",
+          margin: "auto",
+          color: "#489B34",
+        }}
+      >
+        agroform &#169; 2021
+      </footer>
+    </>
+  );
 }
